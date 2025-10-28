23c0d61394f52127059e8c45885cc172df9e68ba
ARS-saad <abdurrahmansaad46@gmail.com>
2025-10-28 09:12:52 +0600

    makeClassification

--- CharacterClassify.java
+++ CharacterClassify.java
@@ -2,15 +2,31 @@ import java.util.Scanner;
 
 public class CharacterClassify {
 
+
+    void makeClassification(String str){
+        int vowel = 0, consonants = 0, digit = 0, specialChar = 0;
+        for (int i = 0; i<str.length(); i++){
+         char ch=str.charAt(i);
+            if(ch>'a' && ch<'z' || ch>'A' && ch<'Z'){
+                
+            }
+        }
+
+    }
+
+
+
     public static void main(String[] args) {
  
+        CharacterClassify cc = new CharacterClassify();
         Scanner input = new Scanner(System.in);
 
 
         System.out.print("Enter a string: ");
-        String str = input.nextLine();
+        String str = input.nextLine(); // Hello@24
 
-        System.out.println( str);
+        System.out.println( str); 
+        cc.makeClassification(str);
 
     }
 }
